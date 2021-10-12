f32bc5ee7ce1e08c8f52559b305efb4357b3ba4b
Guilherme Setim <64696390+guilhermesetim@users.noreply.github.com>
2021-10-12 19:22:20 -0300

    otimização shell script

--- posinstall.sh
+++ posinstall.sh
@@ -10,24 +10,30 @@ sudo apt update &&
 
 ## Instalando pacotes e programas do repositório deb do Ubuntu ##
 
-sudo apt install python3 nodejs git gnome-boxes -y &&
-sudo apt install npm -y;
+sudo apt gnome-boxes -y ;
+
+## Intalando pacotes developer ##
+
+sudo apt install git -y ;
+sudo apt install nodejs npm -y ;
+sudo apt python3 -y ;
+sudo apt install apache2 mysql-server php php-mysql libapache2-mod-php -y ;
+sudo apt install phpmyadmin ;
+
 
 ## Instalando pacotes Snap ##
 
-sudo snap install gimp kdenlive handbrake-jz discord &&
-sudo snap install code --classic &&
+sudo snap install gimp kdenlive handbrake-jz ;
+sudo snap install code --classic ;
+sudo snap install slack --classic ;
 
 ## Instalando configurações de interface gnome ##
-sudo apt install gnome-tweak-tool &&
-sudo apt install gnome-tweaks &&
- 
+sudo apt install gnome-tweak-tool ;
 
 ## Softwares que precisam de download externo ##
 
-cd ~/Downloads/;
-wget -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && 
-wget -c https://github.com/shiftkey/desktop/releases/download/release-2.9.3-linux3/GitHubDesktop-linux-2.9.3-linux3.deb &&
+wget -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb ; 
+wget -c https://github.com/shiftkey/desktop/releases/download/release-2.9.3-linux3/GitHubDesktop-linux-2.9.3-linux3.deb ;
 sudo dpkg -i *.deb &&
 
 ## Atualização do sistema ##
