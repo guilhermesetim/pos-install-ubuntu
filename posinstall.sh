25b4ed60a4f13e612da992d58bd4af1c60c3b1c8
Guilherme Setim <64696390+guilhermesetim@users.noreply.github.com>
2021-11-08 10:26:11 -0300

    composer php
    adicionando as linhas de instalação do Composer (gerenciador de pacotes php)

--- posinstall.sh
+++ posinstall.sh
@@ -20,6 +20,12 @@ sudo apt python3 -y ;
 sudo apt install apache2 mysql-server php php-mysql libapache2-mod-php -y ;
 sudo apt install phpmyadmin -y ;
 
+## Composer (gerenciador de pacote php) ##
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('sha384', 'composer-setup.php') === '906a84df04cea2aa72f40b5f787e49f22d4c2f19492ac310e8cba5b96ac8b64115ac402c8cd292b8a03482574915d1a8') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+
 
 ## Instalando pacotes Snap ##
 
